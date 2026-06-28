b5a6b600b54a81842d8ff6c4b64b8833706e0d67
asuma-app <ditss.business@gmail.com>
2026-06-28 10:47:41 +0700

    Create cors.ts

---
+++ src/middleware/cors.ts
@@ -0,0 +1,18 @@
+import { createMiddleware } from 'hono/factory';
+import { env } from '../config/env';
+
+export const corsMiddleware = createMiddleware(async (c, next) => {
+  const origin = env.CORS_ORIGIN;
+
+  c.header('Access-Control-Allow-Origin', origin);
+  c.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  c.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  c.header('Access-Control-Allow-Credentials', 'true');
+  c.header('Access-Control-Max-Age', '86400');
+
+  if (c.req.method === 'OPTIONS') {
+    return c.text('', 204);
+  }
+
+  await next();
+});
